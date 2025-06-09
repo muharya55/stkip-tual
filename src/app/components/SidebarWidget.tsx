23bcb6ee89a2c440323ea73185bf67456c264f9e
muharya55 <farhansyahputra9901@gmail.com>
2025-06-10 00:42:14 +0700

    perbaikan

--- src/app/components/SidebarWidget.tsx
+++ src/app/components/SidebarWidget.tsx
@@ -1,22 +1,24 @@
+type Item = {
+  title: string;
+  link: string;
+  date: string;
+};
+
 type SidebarWidgetProps = {
   title: string;
-  items: string[];
+  items: Item[];
 };
 
-const SidebarWidget = ({ title, items }: SidebarWidgetProps) => {  
-  return (
-    <div className="sidebar-widget">
-      <h5>{title}</h5>
-      <ul>
-        {items.map((item: any, index: number) => (
-          <li key={index}>
-            <a href={item.link}>{item.title}</a>
-            <div className="date">{item.date}</div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default SidebarWidget;
+const SidebarWidget = ({ title, items }: SidebarWidgetProps) => (
+  <div>
+    <h4>{title}</h4>
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>
+          <a href={item.link}>{item.title}</a>
+          <div className="date">{item.date}</div>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
